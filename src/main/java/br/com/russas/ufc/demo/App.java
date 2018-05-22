56450c13ce6eb8d0fc7b7cb03ab105dc1fe61f20
Evandro vila <vylanova@gmail.com>
2018-05-22 17:54:14 -0300

    commit

---
+++ src/main/java/br/com/russas/ufc/demo/App.java
@@ -0,0 +1,13 @@
+package br.com.russas.ufc.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
